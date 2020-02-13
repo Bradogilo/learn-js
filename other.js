c1e5ee85f2d75a28cc28de58a7cc02bd0fdde148
Braddox Odhiambo <braddoxodhiambo@gmail.com>
2020-02-13 23:20:19 +0300

    CHALLENGE 1
    - create file structure
    - link JavaScript file to the html file
    - log 'Greetings from the other.js file!' to the console

---
+++ other.js
@@ -0,0 +1,12 @@
+/**
+ * JS-BASICS/INTRO/CHALLENGE-1-TASK
+ *
+ * Create a new .js file called "other.js"
+ * and in this file the following line should be printed on the console:
+ *
+ * "Greetings from the other.js file!"
+ *
+ * Add reference to this file to the "index.html"
+ */
+
+console.log('Greetings from the other.js file!');
